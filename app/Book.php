dd12b474de6d264618db887900e8583d27cd95bc
rusdeman <rusdeman@gmail.com>
2021-04-19 10:53:14 +0300

    add fillable

--- app/Book.php
+++ app/Book.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
+
+    protected $fillable = [
+      'author_id',
+      'title'
+    ];
+
     public function scores() {
         return $this->hasMany('App\Score');
     }
