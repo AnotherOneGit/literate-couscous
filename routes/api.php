e880b565727b3281faaa6c1d22f06c98dda9840b
rusdeman <rusdeman@gmail.com>
2021-04-19 10:54:09 +0300

    add paths

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,13 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::get('/authors', 'AuthorController@index');
+Route::get('/authors/{author}', 'AuthorController@show');
+Route::post('/authors', 'AuthorController@store');
 
-Route::get('search', 'BookController@search');
+Route::get('/books', 'BookController@index');
+Route::get('/books/{book}', 'BookController@show');
+Route::post('/books', 'BookController@store');
+
+Route::get('/search', 'BookController@search');
 
 Route::post('/score', 'ScoreController@store');
